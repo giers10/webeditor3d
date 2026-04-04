e99a8ece6684521f362e3fb9f98ad766a23db8f9
Victor Giers <mail@victorgiers.com>
2026-04-04 19:33:55 +0200

    Update editor store tests with initialRotationDegrees and initialSize

--- tests/domain/editor-store.test.ts
+++ tests/domain/editor-store.test.ts
@@ -283,6 +283,16 @@ describe("EditorStore", () => {
             x: 0,
             y: 1,
             z: 0
+          },
+          initialRotationDegrees: {
+            x: 0,
+            y: 0,
+            z: 0
+          },
+          initialSize: {
+            x: 2,
+            y: 2,
+            z: 2
           }
         }
       })
@@ -303,6 +313,16 @@ describe("EditorStore", () => {
           x: 0,
           y: 1,
           z: 0
+        },
+        rotationDegrees: {
+          x: 0,
+          y: 0,
+          z: 0
+        },
+        size: {
+          x: 2,
+          y: 2,
+          z: 2
         }
       }
     });
