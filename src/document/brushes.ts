e32c00e0ef39f863cbd465f18e42b920b45a3eef
Victor Giers <mail@victorgiers.com>
2026-04-07 07:10:36 +0200

    Add surface displacement enabled setting to box brush water settings

--- src/document/brushes.ts
+++ src/document/brushes.ts
@@ -88,6 +88,7 @@ export interface BoxBrushWaterSettings {
   surfaceOpacity: number;
   waveStrength: number;
   foamContactLimit: number;
+  surfaceDisplacementEnabled: boolean;
 }
 
 export interface BoxBrushFogSettings {
@@ -158,7 +159,8 @@ const DEFAULT_BOX_BRUSH_WATER_SETTINGS: BoxBrushWaterSettings = {
   colorHex: "#4da6d9",
   surfaceOpacity: 0.55,
   waveStrength: 0.35,
-  foamContactLimit: DEFAULT_BOX_BRUSH_WATER_FOAM_CONTACT_LIMIT
+  foamContactLimit: DEFAULT_BOX_BRUSH_WATER_FOAM_CONTACT_LIMIT,
+  surfaceDisplacementEnabled: false
 };
 
 const DEFAULT_BOX_BRUSH_FOG_SETTINGS: BoxBrushFogSettings = {
@@ -430,7 +432,8 @@ export function createDefaultBoxBrushWaterSettings(): BoxBrushWaterSettings {
     colorHex: DEFAULT_BOX_BRUSH_WATER_SETTINGS.colorHex,
     surfaceOpacity: DEFAULT_BOX_BRUSH_WATER_SETTINGS.surfaceOpacity,
     waveStrength: DEFAULT_BOX_BRUSH_WATER_SETTINGS.waveStrength,
-    foamContactLimit: DEFAULT_BOX_BRUSH_WATER_SETTINGS.foamContactLimit
+    foamContactLimit: DEFAULT_BOX_BRUSH_WATER_SETTINGS.foamContactLimit,
+    surfaceDisplacementEnabled: DEFAULT_BOX_BRUSH_WATER_SETTINGS.surfaceDisplacementEnabled
   };
 }
 
@@ -461,7 +464,8 @@ export function cloneBoxBrushVolumeSettings(volume: BoxBrushVolumeSettings): Box
           colorHex: volume.water.colorHex,
           surfaceOpacity: volume.water.surfaceOpacity,
           waveStrength: volume.water.waveStrength,
-          foamContactLimit: volume.water.foamContactLimit
+          foamContactLimit: volume.water.foamContactLimit,
+          surfaceDisplacementEnabled: volume.water.surfaceDisplacementEnabled
         }
       };
     case "fog":
