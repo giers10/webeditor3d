f4624d4bc1ed1e4a38220edb40e5640f3a213b09
Victor Giers <mail@victorgiers.com>
2026-04-07 06:56:17 +0200

    Add foamContactLimit to water properties in multiple tests

--- tests/domain/create-box-brush.command.test.ts
+++ tests/domain/create-box-brush.command.test.ts
@@ -307,7 +307,8 @@ describe("box brush commands", () => {
       water: {
         colorHex: "#3a7dc2",
         surfaceOpacity: 0.65,
-        waveStrength: 0.4
+        waveStrength: 0.4,
+        foamContactLimit: 6
       }
     });
 
@@ -322,7 +323,8 @@ describe("box brush commands", () => {
       water: {
         colorHex: "#3a7dc2",
         surfaceOpacity: 0.65,
-        waveStrength: 0.4
+        waveStrength: 0.4,
+        foamContactLimit: 6
       }
     });
   });
