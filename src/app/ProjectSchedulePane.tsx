a45a4eede66c6367f721de8b4c26fd1661e8ca89
Victor Giers <mail@victorgiers.com>
2026-04-14 23:21:35 +0200

    Remove unused function parameter onSetRoutineDays in ProjectSchedulePane.tsx

--- src/app/ProjectSchedulePane.tsx
+++ src/app/ProjectSchedulePane.tsx
@@ -195,7 +195,6 @@ export function ProjectSequencerPane({
   onSetRoutineEndHour,
   onSetRoutinePriority,
   onSetRoutineEffectOption,
-  onSetRoutineDays,
   onSetRoutineNumericValue,
   onSetRoutineColorValue,
   onSetRoutineAnimationClip,
