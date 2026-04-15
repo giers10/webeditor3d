4cd99680d4f951c4fffc23c76f73e96523cd6c71
Victor Giers <mail@victorgiers.com>
2026-04-15 03:54:24 +0200

    Fix scene validation by including scenes in context

--- src/document/scene-document-validation.ts
+++ src/document/scene-document-validation.ts
@@ -4933,7 +4933,7 @@ function validateProjectResources(
     validateProjectSequence(
       sequence,
       path,
-      { dialogues: document.dialogues, scenes: {} },
+      { dialogues: document.dialogues, scenes: document.scenes },
       context,
       diagnostics
     );
@@ -5221,7 +5221,7 @@ export function validateSceneDocument(
     validateProjectSequence(
       sequence,
       path,
-      { dialogues: document.dialogues, scenes: document.scenes },
+      { dialogues: document.dialogues, scenes: {} },
       projectSchedulerValidationContext,
       diagnostics
     );
