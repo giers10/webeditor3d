2feb5799b2fe00d7699d5b74ecfa21e19b15d727
Victor Giers <mail@victorgiers.com>
2026-04-15 09:08:38 +0200

    Update viewport transient state to support cone and torus brushes

--- src/viewport-three/viewport-transient-state.ts
+++ src/viewport-three/viewport-transient-state.ts
@@ -21,6 +21,15 @@ export type CreationTarget =
       sideCount: number;
     }
   | {
+      kind: "cone-brush";
+      sideCount: number;
+    }
+  | {
+      kind: "torus-brush";
+      majorSegmentCount: number;
+      tubeSegmentCount: number;
+    }
+  | {
       kind: "entity";
       entityKind: EntityKind;
       audioAssetId: string | null;
@@ -84,6 +93,17 @@ export function cloneViewportToolPreview(toolPreview: ViewportToolPreview): View
                   kind: "cylinder-brush",
                   sideCount: toolPreview.target.sideCount
                 }
+              : toolPreview.target.kind === "cone-brush"
+                ? {
+                    kind: "cone-brush",
+                    sideCount: toolPreview.target.sideCount
+                  }
+                : toolPreview.target.kind === "torus-brush"
+                  ? {
+                      kind: "torus-brush",
+                      majorSegmentCount: toolPreview.target.majorSegmentCount,
+                      tubeSegmentCount: toolPreview.target.tubeSegmentCount
+                    }
           : {
               kind: "box-brush"
             },
@@ -130,6 +150,23 @@ export function areViewportToolPreviewsEqual(left: ViewportToolPreview, right: V
     left.target.kind === "cylinder-brush" &&
     right.target.kind === "cylinder-brush" &&
     left.target.sideCount !== right.target.sideCount
+  ) {
+    return false;
+  }
+
+  if (
+    left.target.kind === "cone-brush" &&
+    right.target.kind === "cone-brush" &&
+    left.target.sideCount !== right.target.sideCount
+  ) {
+    return false;
+  }
+
+  if (
+    left.target.kind === "torus-brush" &&
+    right.target.kind === "torus-brush" &&
+    (left.target.majorSegmentCount !== right.target.majorSegmentCount ||
+      left.target.tubeSegmentCount !== right.target.tubeSegmentCount)
   ) {
     return false;
   }
