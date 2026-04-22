7b219a9e507ea9e61f975f671de208c9681b15b2
Victor Giers <mail@victorgiers.com>
2026-04-22 18:10:27 +0200

    auto-git:  [change] src/viewport-three/viewport-focus.ts

--- src/viewport-three/viewport-focus.ts
+++ src/viewport-three/viewport-focus.ts
@@ -13,7 +13,10 @@ import type { Brush } from "../document/brushes";
 import type { SceneDocument } from "../document/scene-document";
 import type { ScenePath } from "../document/paths";
 import { getTerrainBounds, type Terrain } from "../document/terrains";
-import type { EntityInstance } from "../entities/entity-instances";
+import {
+  resolveCameraRigDocumentPosition,
+  type EntityInstance
+} from "../entities/entity-instances";
 import type { ModelInstance } from "../assets/model-instances";
 import type { ProjectAssetRecord } from "../assets/project-assets";
 import { getBrushBounds } from "../geometry/whitebox-brush";
