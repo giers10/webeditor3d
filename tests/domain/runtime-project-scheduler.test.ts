f0d18442c2b155e507079da8aa6185263443269a
Victor Giers <mail@victorgiers.com>
2026-04-23 02:38:52 +0200

    auto-git:  [change] tests/domain/runtime-project-scheduler.test.ts

--- tests/domain/runtime-project-scheduler.test.ts
+++ tests/domain/runtime-project-scheduler.test.ts
@@ -5,6 +5,7 @@ import {
   createActivateCameraRigOverrideControlEffect,
   createActorControlTargetRef,
   createCameraRigControlTargetRef,
+  createClearCameraRigOverrideControlEffect,
   createDefaultResolvedControlSource,
   createEmptyRuntimeResolvedControlState,
   createLightControlTargetRef,
