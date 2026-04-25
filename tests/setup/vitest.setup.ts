dfbf6c2a3f45b1c343563468968abaef8d600ad7
Victor Giers <mail@victorgiers.com>
2026-04-25 04:31:49 +0200

    auto-git:  [change] tests/setup/vitest.setup.ts

--- tests/setup/vitest.setup.ts
+++ tests/setup/vitest.setup.ts
@@ -1,1 +1,22 @@
 import "@testing-library/jest-dom/vitest";
+
+if (typeof globalThis.ProgressEvent === "undefined") {
+  class TestProgressEvent extends Event implements ProgressEvent {
+    readonly lengthComputable: boolean;
+    readonly loaded: number;
+    readonly total: number;
+
+    constructor(type: string, eventInitDict: ProgressEventInit = {}) {
+      super(type, eventInitDict);
+      this.lengthComputable = eventInitDict.lengthComputable ?? false;
+      this.loaded = eventInitDict.loaded ?? 0;
+      this.total = eventInitDict.total ?? 0;
+    }
+  }
+
+  Object.defineProperty(globalThis, "ProgressEvent", {
+    configurable: true,
+    writable: true,
+    value: TestProgressEvent
+  });
+}
