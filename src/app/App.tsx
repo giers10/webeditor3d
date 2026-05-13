c20e4d966c0119cbcf1f2f14789a4b729194650e
Victor Giers <mail@victorgiers.com>
2026-05-13 13:28:21 +0200

    Remove redundant calculation of spline corridor junction list

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2818,9 +2818,6 @@ export function App({
     terrainList
   );
   const selectedPath = getSelectedPath(editorState.selection, pathList);
-  const splineCorridorJunctionList = getSplineCorridorJunctions(
-    editorState.document.splineCorridorJunctions
-  );
   const splineCorridorJunctionCandidates = detectSplineCorridorJunctionCandidates({
     paths: pathList,
     junctions: editorState.document.splineCorridorJunctions,
